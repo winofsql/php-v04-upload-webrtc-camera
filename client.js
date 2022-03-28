f94715a4867f72eafc6f76749fcf943be3cbb837
sworc <lightbox@nifty.com>
2022-03-28 10:39:08 +0900

    Update client.js

--- client.js
+++ client.js
@@ -4,7 +4,7 @@
 jQuery.isMobile = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
 toastr.options={"closeButton":false,"debug":false,"newestOnTop":false,"progressBar":false,"positionClass":"toast-bottom-center","preventDuplicates":false,"onclick":null,"showDuration":"300","hideDuration":"1000","timeOut":"3000","extendedTimeOut":"1000","showEasing":"swing","hideEasing":"linear","showMethod":"fadeIn","hideMethod":"fadeOut"};
 if ( !$.isMobile ) {
-	toastr.options.positionClass = "toast-top-center";
+    toastr.options.positionClass = "toast-top-center";
 }
 
 // *************************************
